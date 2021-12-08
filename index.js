5e98224d3f18082573b43d4b179a7ac9802e4cb3
David Forero <davefogo@gmail.com>
2021-12-08 16:08:34 -0600

    Add stepThroughCell implementation

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ World.add(world, walls);
 const grid = Array(cells).fill(null).map(() => Array(cells).fill(false));
 const verticals = Array(cells).fill(null).map(() => Array(cells - 1).fill(false));
 const horizontals = Array(cells - 1).fill(null).map(() => Array(cells).fill(false));
+const startRow = Math.floor(Math.random() * cells);
+const startColumn = Math.floor(Math.random() * cells);
 
-console.log(grid);
-console.log(verticals);
-console.log(horizontals);
+console.log(startRow, startColumn);
